d5386eb096dd8bf8529bf2eb7098391990649543
Norris <im@justnorris.com>
2017-10-25 13:45:16 +0300

    Fix incorrect template part path

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'partials/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
