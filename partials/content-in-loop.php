bcc8feb460b63f9540aaae012419218b54b9fcde
Norris <im@justnorris.com>
2017-11-16 15:34:44 +0200

    Add thumbnails to posts

--- partials/content-in-loop.php
+++ partials/content-in-loop.php
@@ -11,6 +11,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	<?php the_post_thumbnail( 'full' ) ?>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
