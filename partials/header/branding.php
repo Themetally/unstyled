a7e30d95a44a3db8f4d1e0b351d93e37cb208e8f
Norris <im@justnorris.com>
2017-11-17 16:41:02 +0200

    Fix incorrect closing tag

--- partials/header/branding.php
+++ partials/header/branding.php
@@ -3,20 +3,21 @@
 
 	<?php if ( get_bloginfo( 'name' ) || get_bloginfo( 'description' ) ): ?>
 
-	<div class="site-title">
-		<?php if ( is_front_page() && is_home() ) : ?>
-			<h1 class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		<?php else : ?>
-			<div class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
-		<?php endif; ?>
+		<div class="site-title">
+			<?php if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<div class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+			<?php endif; ?>
 
-		<?php
-		$description = get_bloginfo( 'description', 'display' );
-		if ( $description || is_customize_preview() ) : ?>
-			<div class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></div>
 			<?php
-		endif; ?>
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<div class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></div>
+				<?php
+			endif; ?>
+		</div>
 
-		<?php endif; ?>
-	</div>
+	<?php endif; ?>
+
 </div><!-- .site-branding -->
